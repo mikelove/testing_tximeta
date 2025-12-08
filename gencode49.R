f5ec31466d8de4ed83a08b85ed8c69e26fc78410
Mike Love <mikelove@users.noreply.github.com>
2025-12-08 16:35:58 -0500

    update

--- gencode49.R
+++ gencode49.R
@@ -1,2 +1,11 @@
 # this is for testing GENCODE 49 related issue
 # https://github.com/thelovelab/tximeta/issues/95
+
+library(tximeta)
+se <- tximeta(
+  coldata = coldata,
+  type = "salmon",
+  txOut = TRUE,
+  markDuplicateTxps = TRUE,
+  cleanDuplicateTxps = TRUE
+)
