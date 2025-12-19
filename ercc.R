609ba9ef0363a452aee33606bc8f3c16154de641
Mike Love <mikelove@users.noreply.github.com>
2025-12-19 09:02:18 -0500

    new stuff

--- ercc.R
+++ ercc.R
@@ -1,2 +1,13 @@
 # this is for testing ERCC data from Charlotte
-#
+
+library(tximeta)
+coldata <- data.frame(
+    names = "s1", 
+    files = "ercc/oarfish-ercc-out/s1.quant"
+)
+se <- importData(
+    coldata, 
+    type = "oarfish",
+    dropInfReps=TRUE
+)
+inspectDigests(se)
